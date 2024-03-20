eecb4051d3a78f5b90836498a187772ce79634f6
weebel_inator <>
2024-03-19 18:44:26 -0700

    light node code working

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,17 @@
 
 #include "painlessMesh.h"
 
-#define NODE_TYPE_SWITCH
-// #define NODE_TYPE_LIGHT
+// #define NODE_TYPE_SWITCH
+#define NODE_TYPE_LIGHT
 
 #define   MESH_PREFIX     "simpopus"
 #define   MESH_PASSWORD   "DoNotMessWithMeGirl"
 // #define   MESH_PREFIX     "whateverYouLike"
 // #define   MESH_PASSWORD   "somethingSneaky"
 #define   MESH_PORT       5555
+
+#define ON_MESSAGE "Get to work!!"
+#define OFF_MESSAGE "Take a Nap!!"
 
 #ifdef NODE_TYPE_SWITCH
 #define   SWITCH_1_PIN    D1
@@ -29,8 +32,7 @@
 Scheduler userScheduler; // to control your personal task
 painlessMesh  mesh;
 
-bool lightOn = false;
-
+#ifdef NODE_TYPE_SWITCH
 // User stub
 void readSwitches() ; // Prototype so PlatformIO doesn't complain
 void turnOn() ; // Prototype so PlatformIO doesn't complain
@@ -41,7 +43,7 @@ Task taskTurnOn( TASK_SECOND * 1 , TASK_FOREVER, &turnOn );
 Task taskTurnOff( TASK_SECOND * 1 , TASK_FOREVER, &turnOff );
 
 void readSwitches() {
-  if(digitalRead(D1) == LOW) {
+  if(digitalRead(SWITCH_1_PIN) == LOW) {
     // lightOn = true;
     taskTurnOn.enableIfNot();
     taskTurnOff.disable();
@@ -53,7 +55,7 @@ void readSwitches() {
 }
 
 void turnOnNode(int nodeId) {
-  String msg = "Get to work!!";
+  String msg = ON_MESSAGE;
   msg += mesh.getNodeId();
   mesh.sendBroadcast( msg );
   // taskTurnOn.disable();
@@ -64,7 +66,7 @@ void turnOn() {
 }
 
 void turnOffNode(int nodeId) {
-  String msg = "Take a Nap!!";
+  String msg = OFF_MESSAGE;
   msg += mesh.getNodeId();
   mesh.sendBroadcast( msg );
   // taskTurnOff.disable();
@@ -80,13 +82,6 @@ void receivedCallback( uint32_t from, String &msg ) {
   Serial.printf("pineapple: Received from %u msg=%s\n", from, msg.c_str());
 }
 
-void receivedCallbackEcho( uint32_t from, String &msg ) {
-  Serial.printf("echoNode: Received from %u msg=%s\n", from, msg.c_str());
-  // String testMsg = "Switch is Now On";
-  // mesh.sendSingle(from, testMsg);
-  mesh.sendSingle(from, msg);
-}
-
 void newConnectionCallback(uint32_t nodeId) {
     Serial.printf("--> pineapple: New Connection, nodeId = %u\n", nodeId);
 }
@@ -98,12 +93,40 @@ void changedConnectionCallback() {
 void nodeTimeAdjustedCallback(int32_t offset) {
     Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
 }
+#endif
+
+#ifdef NODE_TYPE_LIGHT
+void receivedCallbackEcho( uint32_t from, String &msg ) {
+  Serial.printf("echoNode: Received from %u msg=%s\n", from, msg.c_str());
+  // String testMsg = "Switch is Now On";
+  // mesh.sendSingle(from, testMsg);
+  mesh.sendSingle(from, msg);
+}
+
+void receivedCallbackHandleLight( uint32_t from, String &msg ) {
+  Serial.printf("echoNode: Received from %u msg=%s\n", from, msg.c_str());
+  if (msg.indexOf(ON_MESSAGE) >= 0) {
+    digitalWrite(LIGHT_1_PIN, HIGH);
+    // digitalWrite(LIGHT_2_PIN, HIGH);
+    Serial.println("Switch is Now On");
+  } else if (msg.indexOf(OFF_MESSAGE) >= 0) {
+    digitalWrite(LIGHT_1_PIN, LOW);
+    // digitalWrite(LIGHT_2_PIN, LOW);
+    Serial.println("Switch is Now Off");
+  } else {
+    Serial.println("Unknown message");
+  }
+  // String testMsg = "Switch is Now On";
+  // mesh.sendSingle(from, testMsg);
+  mesh.sendSingle(from, msg);
+}
+#endif
 
 void setup() {
   Serial.begin(115200);
 
 #ifdef NODE_TYPE_SWITCH
-  pinMode(D1, INPUT_PULLUP);
+  pinMode(SWITCH_1_PIN, INPUT_PULLUP);
 
 //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
   mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
@@ -121,15 +144,29 @@ void setup() {
 #endif
 
 #ifdef NODE_TYPE_LIGHT
-  pinMode(D1, OUTPUT);
-  digitalWrite(D1, LOW);
-  pinMode(D2, OUTPUT);
-  digitalWrite(D2, LOW);
+  pinMode(LIGHT_1_PIN, OUTPUT);
+  digitalWrite(LIGHT_1_PIN, LOW);
+  // pinMode(LIGHT_2_PIN, OUTPUT);
+  // digitalWrite(LIGHT_2_PIN, LOW);
+
+  delay(500);
+  digitalWrite(LIGHT_1_PIN, HIGH);
+  // digitalWrite(LIGHT_2_PIN, HIGH);
+  delay(500);
+  digitalWrite(LIGHT_1_PIN, LOW);
+  // digitalWrite(LIGHT_2_PIN, LOW);
+  delay(500);
+  digitalWrite(LIGHT_1_PIN, HIGH);
+  // digitalWrite(LIGHT_2_PIN, HIGH);
+  delay(500);
+  digitalWrite(LIGHT_1_PIN, LOW);
+  // digitalWrite(LIGHT_2_PIN, LOW);
+
 
   mesh.setDebugMsgTypes( ERROR | STARTUP | CONNECTION );  // set before init() so that you can see startup messages
 
   mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT );
-  mesh.onReceive(&receivedCallbackEcho);
+  mesh.onReceive(&receivedCallbackHandleLight);
 #endif
 }
 
